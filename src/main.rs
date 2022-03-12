250cfb52c693f11215937366a2cd4234c420be7c
Rainux Luo <rainux@gmail.com>
2022-03-13 06:46:40 +0800

    Basically works!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,127 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use kuchiki::traits::TendrilSink;
+use std::fs::{self, File};
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() -> Result<()> {
+    fs::create_dir_all("lyrics")?;
+
+    if let Ok(lines) = read_lines("songs") {
+        for line in lines {
+            if let Ok(song) = line {
+                if let Some(url) = search_song(&song)? {
+                    println!("{}", url);
+                    download_lyric(&url, &song)?;
+                } else {
+                    println!("Not found: {}", song);
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn parse_song_name(song: &str) -> (&str, &str) {
+    if let Some((title, artist)) = song.split_once(" / ") {
+        dbg!(title, artist);
+        (title, artist)
+    } else {
+        dbg!(song);
+        (song, "")
+    }
+}
+
+fn search_song(song: &str) -> Result<Option<String>> {
+    let (title, artist) = parse_song_name(song);
+
+    let body = reqwest::blocking::Client::new()
+        .get("https://utaten.com/lyric/search")
+        .query(&[("artist_name", artist), ("title", title)])
+        .send()?
+        .text()?;
+
+    let document = kuchiki::parse_html().one(body);
+
+    if let Some(link) = document.select(".searchResult__title a").unwrap().next() {
+        let attrs = link.as_node().as_element().unwrap().attributes.borrow();
+        let path = attrs.get("href").unwrap();
+        let url = format!("https://utaten.com{}", path);
+        Ok(Some(url))
+    } else {
+        Ok(None)
+    }
+}
+
+fn download_lyric(url: &str, song: &str) -> Result<()> {
+    let body = reqwest::blocking::Client::new().get(url).send()?.text()?;
+
+    let document = kuchiki::parse_html().one(body);
+
+    let lyric_title = document.select(".newLyricTitle").unwrap().next().unwrap();
+    let lyric_title = lyric_title.as_node();
+    // Remove "の歌詞" in title
+    lyric_title
+        .select(".newLyricTitle_afterTxt")
+        .unwrap()
+        .next()
+        .unwrap()
+        .as_node()
+        .detach();
+
+    let lyric_data = document.select(".lyricData").unwrap().next().unwrap();
+    let lyric_data = lyric_data.as_node();
+    // # Remove tags and action buttons
+    lyric_data
+        .select(".newLyricWorkFooter")
+        .unwrap()
+        .next()
+        .unwrap()
+        .as_node()
+        .detach();
+    // Fix relative links
+    lyric_data
+        .select(".newLyricWork a")
+        .unwrap()
+        .for_each(|link| {
+            let mut attrs = link.as_node().as_element().unwrap().attributes.borrow_mut();
+            if let Some(href) = attrs.get_mut("href") {
+                *href = format!("https://utaten.com{}", href);
+            }
+        });
+
+    let lyric_body = document.select(".lyricBody").unwrap().next().unwrap();
+    let lyric_body = lyric_body.as_node();
+    // Remove romaji part
+    lyric_body
+        .select(".romaji")
+        .unwrap()
+        .next()
+        .unwrap()
+        .as_node()
+        .detach();
+
+    let article = document.select("article").unwrap().next().unwrap();
+    let article = article.as_node();
+    article.children().for_each(|c| c.detach());
+    article.append(lyric_title.to_owned());
+    article.append(lyric_data.to_owned());
+    article.append(lyric_body.to_owned());
+
+    let mut html = Vec::new();
+    article.serialize(&mut html)?;
+
+    let filename = format!("lyrics/{}.html", song.replace(" / ", " - "));
+    fs::write(filename, html)?;
+
+    Ok(())
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
